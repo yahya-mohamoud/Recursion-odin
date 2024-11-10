4d244fe1bd0f72771a02fa793d8fe23da34cb749
yahya-mohamoud <yahyamohamoud625@gmail.com.com>
2024-11-10 08:00:25 +0300

    finished the fibonacci sequence

---
+++ fibonacci.js
@@ -0,0 +1,31 @@
+function fibs (num) {
+    let numOne = 0;
+    let numTwo = 1;
+    let numTot;
+
+    for(i = 1; i <= num; i++) {
+        console.log(numOne);
+        numTot = numOne + numTwo;
+        numOne = numTwo
+        numTwo = numTot
+    }
+}
+
+fibs(7)
+
+// using recursion method
+
+function fibsRec (num) {
+    if (num == 0) return [0];
+    if (num == 1) return [0, 1];
+    
+    const arr = fibsRec(num - 1)
+    return [...arr, arr[num- 1] + arr[num- 2]]
+}
+
+console.log(fibsRec(8));
+
+
+
+
+
